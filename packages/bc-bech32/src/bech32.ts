87e3a6dc63f5ab1eba7e2716e000490913e7a8ba
aaronisme <aarondongchen@gmail.com>
2020-05-21 22:29:07 +0800

    add the bc-bech32

---
+++ packages/bc-bech32/src/bech32.ts
@@ -0,0 +1,135 @@
+import { Bech32Version } from './index';
+
+const CHARSET = 'qpzry9x8gf2tvdw0s3jn54khce6mua7l';
+const GENERATOR = [0x3b6a57b2, 0x26508e6d, 0x1ea119fa, 0x3d4233dd, 0x2a1462b3];
+
+function polymod(values: number[]) {
+    let chk = 1;
+    for (let p = 0; p < values.length; ++p) {
+        const top = chk >> 25;
+        chk = ((chk & 0x1ffffff) << 5) ^ values[p];
+        for (let i = 0; i < 5; ++i) {
+            if ((top >> i) & 1) {
+                chk ^= GENERATOR[i];
+            }
+        }
+        return chk;
+    }
+}
+
+function hrpExpand(hrp: string) {
+    const ret: number[] = [];
+    let p: number;
+    for (p = 0; p < hrp.length; ++p) {
+        ret.push(hrp.charCodeAt(p) >> 5);
+    }
+    ret.push(0);
+    for (p = 0; p < hrp.length; ++p) {
+        ret.push(hrp.charCodeAt(p) & 31);
+    }
+    return ret;
+}
+
+function verifyChecksum(hrp: string | undefined, data: number[], version: Bech32Version) {
+    let header: number[];
+    if (hrp) {
+        header = hrpExpand(hrp);
+    } else {
+        header = [0];
+    }
+    const check = version === Bech32Version.Origin ? 1 : 0x3fffffff;
+    return polymod(header.concat(data)) === check;
+}
+
+function createChecksum(hrp: string | undefined, data: number[], bech32Version: Bech32Version): number[] {
+    let values: number[];
+    if (hrp) {
+        values = hrpExpand(hrp).concat(data).concat([0, 0, 0, 0, 0, 0]);
+    } else {
+        values = [0].concat(data).concat([0, 0, 0, 0, 0, 0]);
+    }
+
+    const chk = bech32Version === Bech32Version.Origin ? 1 : 0x3fffffff;
+
+    const mod = polymod(values) ^ chk;
+    const ret = [];
+    for (let p = 0; p < 6; ++p) {
+        ret.push((mod >> (5 * (5 - p))) & 31);
+    }
+    return ret;
+}
+
+const encode = (hrp: string | undefined, data: number[], version: Bech32Version): string => {
+    const combined = data.concat(createChecksum(hrp, data, version));
+    let ret: string;
+    if (hrp) {
+        ret = hrp + '1';
+    } else {
+        ret = '';
+    }
+    for (let p = 0; p < combined.length; ++p) {
+        ret += CHARSET.charAt(combined[p]);
+    }
+    return ret;
+};
+
+const decodeBc32 = (bechString: string): { hrp: null; data: number[] } => {
+    const data: number[] = [];
+    for (let p = 0; p < bechString.length; ++p) {
+        const d = CHARSET.indexOf(bechString.charAt(p));
+        if (d === -1) {
+            return null;
+        }
+        data.push(d);
+    }
+    if (!verifyChecksum(null, data, Bech32Version.bis)) {
+        return null;
+    }
+    return { hrp: null, data: data.slice(0, data.length - 6) };
+};
+
+const decode = (bechString: string) => {
+    let p: number;
+    let hasLower = false;
+    let hasUpper = false;
+    for (p = 0; p < bechString.length; ++p) {
+        if (bechString.charCodeAt(p) < 33 || bechString.charCodeAt(p) > 126) {
+            return null;
+        }
+        if (bechString.charCodeAt(p) >= 97 && bechString.charCodeAt(p) <= 122) {
+            hasLower = true;
+        }
+        if (bechString.charCodeAt(p) >= 65 && bechString.charCodeAt(p) <= 90) {
+            hasUpper = true;
+        }
+    }
+    if (hasLower && hasUpper) {
+        return null;
+    }
+    bechString = bechString.toLowerCase();
+    const pos = bechString.lastIndexOf('1');
+    if (pos === -1) {
+        return decodeBc32(bechString);
+    }
+    if (pos < 1 || pos + 7 > bechString.length || bechString.length > 90) {
+        return null;
+    }
+    const hrp = bechString.substring(0, pos);
+    const data: number[] = [];
+    for (p = pos + 1; p < bechString.length; ++p) {
+        const d = CHARSET.indexOf(bechString.charAt(p));
+        if (d === -1) {
+            return null;
+        }
+        data.push(d);
+    }
+    if (!verifyChecksum(hrp, data, Bech32Version.Origin)) {
+        return null;
+    }
+    return { hrp: hrp, data: data.slice(0, data.length - 6) };
+};
+
+export default {
+    encode,
+    decode,
+};
---
+++ packages/bc-bech32/src/index.ts
@@ -0,0 +1,71 @@
+import bech32 from './bech32';
+
+export enum Bech32Version {
+    Origin = 1,
+    bis = 2,
+}
+
+function convertbits(data: Uint8Array, frombits: number, tobits: number, pad: boolean): number[] {
+    let acc = 0;
+    let bits = 0;
+    const ret = [];
+    const maxv = (1 << tobits) - 1;
+    for (let p = 0; p < data.length; ++p) {
+        const value = data[p];
+        if (value < 0 || value >> frombits !== 0) {
+            return null;
+        }
+        acc = (acc << frombits) | value;
+        bits += frombits;
+        while (bits >= tobits) {
+            bits -= tobits;
+            ret.push((acc >> bits) & maxv);
+        }
+    }
+    if (pad) {
+        if (bits > 0) {
+            ret.push((acc << (tobits - bits)) & maxv);
+        }
+    } else if (bits >= frombits || (acc << (tobits - bits)) & maxv) {
+        return null;
+    }
+    return ret;
+}
+
+export function decodeSegwitAddress(hrp: string, addr: string) {
+    const dec = bech32.decode(addr);
+    if (dec === null || dec.hrp !== hrp || dec.data.length < 1 || dec.data[0] > 16) {
+        return null;
+    }
+    const res = convertbits(Uint8Array.from(dec.data.slice(1)), 5, 8, false);
+    if (res === null || res.length < 2 || res.length > 40) {
+        return null;
+    }
+    if (dec.data[0] === 0 && res.length !== 20 && res.length !== 32) {
+        return null;
+    }
+    return { version: dec.data[0], program: res };
+}
+
+export function encodeSegwitAddress(hrp, version, program) {
+    const ret = bech32.encode(hrp, [version].concat(convertbits(program, 8, 5, true), Bech32Version.Origin));
+    if (decodeSegwitAddress(hrp, ret) === null) {
+        return null;
+    }
+    return ret;
+}
+
+export function encodeBc32Data(hex: string): string {
+    const data = Buffer.from(hex, 'hex');
+    const ret = bech32.encode(null, convertbits(data, 8, 5, true), Bech32Version.bis);
+    return ret;
+}
+
+export function decodeBc32Data(data: string): null | string {
+    const result = bech32.decode(data);
+    if (result) {
+        return Buffer.from(result.data).toString('hex');
+    } else {
+        return null;
+    }
+}
