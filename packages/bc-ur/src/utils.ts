4bd30c0d6d0063fb3866de2bbe0ba9ee335e4dd2
aaronisme <aarondongchen@gmail.com>
2020-10-14 15:51:03 +0800

    remove dependencies on root project

--- packages/bc-ur/src/utils.ts
+++ packages/bc-ur/src/utils.ts
@@ -1,7 +1,7 @@
-import { crypto } from 'bitcoinjs-lib';
+import shajs from 'sha.js';
 
 export const sha256Hash = (data: Buffer) => {
-    return crypto.sha256(data);
+    return shajs('sha256').update(data).digest();
 };
 
 export const compose3 = (f: Function, g: Function, h: Function) => (x: any) => {
