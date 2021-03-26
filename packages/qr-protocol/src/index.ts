36017012c7a30b527ddbd5d57674660cbfff67e2
soralit <18829236489@163.com>
2021-03-26 10:31:49 +0800

    export UR

--- packages/qr-protocol/src/index.ts
+++ packages/qr-protocol/src/index.ts
@@ -1,3 +1,3 @@
 export { protoc as QRProtocol } from './QRProtocolBuf/proto';
-export { V1, V2 } from './QRProtocolCodec';
+export { V1, V2, UR } from './QRProtocolCodec';
 export type { AnimatedQRDataTypeV1 } from './QRProtocolCodec/types';
